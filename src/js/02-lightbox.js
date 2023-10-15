bca1a53b9e1aa87914ae30a8df87a599e1dfdaea
YuriiPotanin <potaninu@i.ua>
2023-10-15 21:06:35 +0300

    update

---
+++ src/js/02-lightbox.js
@@ -0,0 +1,35 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+console.log(galleryItems);
+
+const imgGallery = document.querySelector(".gallery");
+const createMarkup = galleryItems.map(
+    ({ preview, original, description }) => `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+        </li> `
+    )
+    .join(" ");
+
+imgGallery.innerHTML = createMarkup;
+
+
+const gallery = new SimpleLightbox('.gallery a',{
+    captions: true,
+    enableKeyboard: true,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250
+    
+});
